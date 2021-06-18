9c5e04aab6c98fe228fa24c3599428558ec34ea5
Lucas Amaral <luc.bamaral@gmail.com>
2021-06-17 21:38:46 -0300

    Create breaking_a_number.java

---
+++ breaking_a_number.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		Scanner input = new Scanner(System.in);
+	    double number = input.nextDouble();
+		System.out.println(Math.floor(number));
+	}
+}
